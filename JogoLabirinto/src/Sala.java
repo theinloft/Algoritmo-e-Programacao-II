744e77cc8bf372f1639201a3f5fa2d8dca9448f2
Thiago Einloft <theinloft@gmail.com>
2017-06-18 20:47:41 -0300

    Novas Alterações

---
+++ JogoLabirinto/src/Sala.java
@@ -0,0 +1,25 @@
+import com.senac.SimpleJava.Graphics.Image;
+
+public class Sala {
+	private String nome;
+	Image backgrd;
+	
+	public Sala(String nome) {
+		
+		this.nome = nome;
+		adicionarImagem();
+	}
+
+	private void adicionarImagem() {
+		try {
+			this.backgrd = new Image("Imagens/cenario.png");
+		} catch (Exception e) {
+			
+		}
+		
+	}
+
+	
+	
+	
+}
